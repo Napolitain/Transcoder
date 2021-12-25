5e12837f1915253787ded0ff9945ba4c700445d6
Maxime Boucher <mxboucher@gmail.com>
2021-12-25 14:44:32 +0100

    Initial Commit

---
+++ Shared/TranscoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranscoderApp.swift
+//  Shared
+//
+//  Created by Maxime Boucher on 25/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TranscoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
